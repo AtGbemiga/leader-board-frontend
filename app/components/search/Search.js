4c30606dbdcb69d66306d1ff653e44298f03b7c3
Gbemiga Atolagbe <gbatolagbe@gmail.com>
2023-06-01 16:10:52 +0100

    feat2 update

--- app/components/search/Search.js
+++ app/components/search/Search.js
@@ -6,10 +6,9 @@ export const Search = ({ setResults }) => {
   const [input, setInput] = useState("");
 
   const fetchData = async (value) => {
+    const url = "https://leader-board-backend.vercel.app/api/v1/score/";
     try {
-      const { data } = await axios.get(
-        "https://leader-board-backend.vercel.app/api/v1/score/"
-      );
+      const { data } = await axios.get(url);
       const result = data.filter((user) => {
         return (
           value && user && user.name && user.name.toLowerCase().includes(value)
@@ -26,13 +25,25 @@ export const Search = ({ setResults }) => {
     fetchData(value);
   };
   return (
-    <div>
-      <input
-        type="search"
-        placeholder="search"
-        value={input}
-        onChange={(e) => handleChange(e.target.value)}
-      />
-    </div>
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-search"
+        viewBox="0 0 16 16"
+      >
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+      </svg>
+      <div>
+        <input
+          type="search"
+          placeholder="search"
+          value={input}
+          onChange={(e) => handleChange(e.target.value)}
+        />
+      </div>
+    </>
   );
 };
