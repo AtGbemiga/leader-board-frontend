cc63f9f9776779f164d50a02d5be77dfbc78ca8c
Gbemiga Atolagbe <gbatolagbe@gmail.com>
2023-06-02 03:46:46 +0100

    Add search styles

--- app/components/search/Search.js
+++ app/components/search/Search.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 
 export const Search = ({ setResults }) => {
   const [input, setInput] = useState("");
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(true);
+  const [searchBox, setSearchBox] = useState(true);
+  const searchRef = useRef(null);
   const data = useSelector((store) => store.score.details);
 
   const fetchData = (value) => {
@@ -37,29 +39,53 @@ export const Search = ({ setResults }) => {
     setInput(value);
     fetchData(value);
   };
+
+  const handleSvgClick = () => {
+    setSearchBox((prevState) => !prevState);
+    setInput("");
+    if (searchRef.current) {
+      searchRef.current.focus();
+    }
+  };
+
+  useEffect(() => {
+    if (searchBox && searchRef.current) {
+      searchRef.current.focus();
+    }
+  }, [searchBox]);
+
   return (
-    <>
-      {error && (
-        <p className="text-danger">Enter number or letter, not both.</p>
-      )}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        class="bi bi-search"
-        viewBox="0 0 16 16"
-      >
-        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-      </svg>
-      <div>
-        <input
-          type="search"
-          placeholder="search"
-          value={input}
-          onChange={(e) => handleChange(e.target.value)}
-        />
+    <div className="mt-2">
+      <div className="d-flex align-items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-search"
+          viewBox="0 0 16 16"
+          onClick={handleSvgClick}
+          cursor="pointer"
+        >
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+        </svg>
+        <div className="ps-2 col-4">
+          {searchBox && (
+            <input
+              type="search"
+              placeholder="search"
+              value={input}
+              onChange={(e) => handleChange(e.target.value)}
+              ref={searchRef}
+              className="form-control rounded-pill col-4"
+              style={{ textIndent: 10 }}
+            />
+          )}
+        </div>
       </div>
-    </>
+      {error && (
+        <p className="text-danger m-0 p-0">Enter number or letter, not both.</p>
+      )}
+    </div>
   );
 };
